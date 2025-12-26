3c6a6265e4e0533224c4372c1d9e5c9ea4ab0175
kanix1506 <kanittha_thaotan@cmu.ac.th>
2025-12-26 14:31:54 +0700

    lab10_commit

--- lab10_2.cpp
+++ lab10_2.cpp
@@ -4,15 +4,22 @@
 using namespace std;
 
 int main (){
-	ifstream source;
-	ofstream dest;
-	source.open("cheerbook.txt") ;
-	dest.open("cheerbook_copy.txt");
-	
-	//กรอก Code เฉพาะส่วนนี้ ให้สอดคล้องกับโจทย์และ Code ส่วนอื่น ๆ 
-	//โดยห้ามแก้ไข Code ในส่วนอื่น ๆ
+ ifstream source;
+ ofstream dest;
+ source.open("cheerbook.txt");
+ dest.open("cheerbook_copy.txt");
+ 
+ cout<<"-------------------- BOOM ---------------------"<<"\n";
+ 
+ string line;
+ while(getline(source,line)){
+     cout<<line<<"\n";
+ }
+ 
+ cout<<"-------------------- HA!! ---------------------";
+ 
 
     source.close();
     dest.close();
-	return 0;
+ return 0;
 }
